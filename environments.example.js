9ed7fdd019d794996fb42e5db4312334bd2d16b8
MaxiSuppes <maxisuppes@hotmail.com.ar>
2021-10-21 15:25:11 -0300

    Changed some random styles

---
+++ environments.example.js
@@ -0,0 +1,25 @@
+import * as Updates from "expo-updates";
+
+const ENV = {
+    dev: {
+        apiUrl: '',
+        usingFakeApi: true,
+    },
+    staging: {
+        apiUrl: '',
+    },
+    production: {
+        apiUrl: '',
+    }
+};
+
+export function getEnvVars(env = Updates.releaseChannel) {
+    // Release channels: https://docs.expo.dev/distribution/release-channels/
+    if (env.startsWith('staging')) {
+        return ENV.staging;
+    } else if (env.startsWith('production')) {
+        return ENV.production;
+    } else {
+        return ENV.dev;
+    }
+}
